7b311b6995cf0c726b2f210ad36052ac44a13d35
Michal <michal.zaczek@interia.pl>
2025-09-24 14:35:54 +0200

    feat: load .env variables

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+
 def main():
     print("Hello from ai-agent!")
+    print(api_key)
 
 
 if __name__ == "__main__":
