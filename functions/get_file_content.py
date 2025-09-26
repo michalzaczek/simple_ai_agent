26585ce325dede3f09017729ca7ac27b21902ab2
Michal <michal.zaczek@interia.pl>
2025-09-26 13:12:15 +0200

    feat: add exceptions to get_file_content function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,1 +1,19 @@
+import os
+from functions.file_utils import get_abs_full_path, is_path_inside_wd
+
+
 def get_file_content(working_directory, file_path):
+    try:
+        abs_full_path = get_abs_full_path(working_directory, file_path)
+        if not is_path_inside_wd(working_directory, abs_full_path):
+            raise Exception(
+                f'Cannot read "{file_path}" as it is outside the permitted working directory'
+            )
+
+        if not os.path.isfile(abs_full_path):
+            raise Exception(f'File not found or is not a regular file: "{file_path}"')
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
+print(get_file_content())
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -7,10 +7,12 @@ def get_files_info(working_directory, directory="."):
         abs_full_path = get_abs_full_path(working_directory, directory)
 
         if not is_path_inside_wd(working_directory, abs_full_path):
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            raise Exception(
+                f'Cannot list "{directory}" as it is outside the permitted working directory'
+            )
 
         if not os.path.isdir(abs_full_path):
-            return f'Error: "{directory}" is not a directory'
+            raise Exception(f'"{directory}" is not a directory')
 
         return list_dir_to_str(abs_full_path)
     except Exception as e:
